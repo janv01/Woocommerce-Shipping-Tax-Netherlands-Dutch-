2766e09bc77c2843af17081ccac0b4a650af3d6a
janv01 <86680894+janv01@users.noreply.github.com>
2021-06-30 09:08:04 +0200

    Update Calculate_NL_Shipping_Tax.php

--- Calculate_NL_Shipping_Tax.php
+++ Calculate_NL_Shipping_Tax.php
@@ -13,10 +13,12 @@ class NL_Tax {
 
         public function Calculate_NL_Shipping_Tax()
         {
-       	$total_price_nonstandard_tax = 0;
+       		$total_price_nonstandard_tax = 0;
 	   	$total_price_standard_tax = 0;
 
 		$taxperc = 0.00;
+
+		/* Scan cart-items */
 							
 		foreach ( WC()->cart->get_cart() as $cart_item ) {
 
@@ -26,13 +28,20 @@ class NL_Tax {
 			$tax_class = $cart_item['data']->get_tax_class();
 			$taxrates = WC_Tax::get_rates($tax_class);
 			
+			/* Find taxrate current cart-item */
+
 			for ($i = 0; $i < 10; $i++) {
-    			$taxrate = $taxrates[$i][rate];
+    				$taxrate = $taxrates[$i][rate];
 				if( $taxrate > 0 ) break;
-			}			
-			
+			}
+				
+			/* Calculate tax amount current cart-item */
+				
 			$taxprice = $price * ( 100 / ( 100 + $taxrate )) * $quantity;
 			
+				
+			/* Summarize tax price, split into standard and non-standard tax */
+				
 	   		if( $tax_class == "") { 
 				$taxrate_standard = $taxrate;
 				$total_price_standard_tax += $taxprice; 
@@ -42,6 +51,9 @@ class NL_Tax {
 				$total_price_nonstandard_tax += $taxprice;
 			}
 		}
+				
+		/* At this point taxrates are known for both standard and non-standard tax     */
+		/* Shipping tax percentage can now be calculated using Dutch legislation rules */
 			
 		$taxperc = 	( $total_price_standard_tax / ( $total_price_standard_tax + $total_price_nonstandard_tax ) * $taxrate_standard ) + 
 				( $total_price_nonstandard_tax / ( $total_price_standard_tax + $total_price_nonstandard_tax ) * $taxrate_nonstandard ); 
